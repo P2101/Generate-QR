a88a145ad2ce2812b3569d30fb1ea22e8dacb3ba
P2101 <98814765+P2101@users.noreply.github.com>
2023-03-16 20:54:07 +0100

    Update qr-code.py

--- qr-code.py
+++ qr-code.py
@@ -1,8 +1,8 @@
 import pyqrcode # pip install pyqrcode
 from PIL import Image # IMPORT pip install pypng
 
-name = input('Enter a name for the QR: ')
-link = input('Enter the link to generate the QR: ')
+name = input('Enter a name for the QR: ') # name of the QR image
+link = input('Enter the link to generate the QR: ') # the link you want to convert
 qrcode = pyqrcode.create(link)
 qrcode.png(name + ".png", scale=5)
 Image.open("QRCode.png")
